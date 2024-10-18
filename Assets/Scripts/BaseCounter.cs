422c09f7dd96fa783df2fb96534a0942121fe1b1
Briant Anaya <briantanaya@yahoo.com>
2024-10-17 17:13:36 -0700

    Created parent counter type, and rafactored interact function to be able to interact with any counter type

--- Assets/Scripts/BaseCounter.cs
+++ Assets/Scripts/BaseCounter.cs
@@ -1,11 +1,40 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseCounter : MonoBehaviour
+public class BaseCounter : MonoBehaviour, IKitchenObjectParent
 {
-  public virtual void Interact(Player player)
-    {
+    [SerializeField] private Transform counterTopPoint;
+    private KitchenObject kitchenObject;
 
+    public virtual void Interact(Player player)
+    {
+        Debug.LogError("BaseCounter.Interact");
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) // We want to be able to transfor the proper kitchenObject to the new parent clear counter
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() // We want to check if a clear counter has a kitchen object on top of it
+    {
+        return kitchenObject != null;
     }
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,52 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContainerCounter : BaseCounter,IKitchenObjectParent
+public class ContainerCounter : BaseCounter
 {
+    public event EventHandler OnPlayerGrabObject;
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
 
-    private KitchenObject kitchenObject;
-
-    public void Interact(Player player)
+    public override void Interact(Player player)
     {
-        if (kitchenObject == null) // If kitchenObject does not exist, aka null, then we spawn it and then assign it. Makes it so that we do not spawn an infinite amount of objects just from interacting with the counters multiple times if the object exists.
+        if (!HasKitchenObject()) // If kitchenObject does not exist, aka null, then we spawn it and then assign it. Makes it so that we do not spawn an infinite amount of objects just from interacting with the counters multiple times if the object exists.
         {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint); // Create or spawn an object of the tomato prefab using the position of an empty game object (which should be set somewhere desirable for the tomato to spawn) 
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this); // We set the clear counter and the visual for the kitchen object on it (Further reference in KitchenObject.cs)
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab); // Create or spawn an object of the tomato prefab using the position of an empty game object (which should be set somewhere desirable for the tomato to spawn) 
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player); // We set the clear counter and the visual for the kitchen object on it (Further reference in KitchenObject.cs)
+
+            OnPlayerGrabObject?.Invoke(this, EventArgs.Empty);
         }
-        else // If the kitchen object is spawnned in or already  exists,  then we just look at which clear counter it is tied to or referencing
-        {
-            // Give object to the player
-            kitchenObject.SetKitchenObjectParent(player);
-            Debug.Log(kitchenObject.GetKitchenObjectParent());
-        }
-        //Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName); // This calls the GetKitchentObjectSO method inside the KitchenObject Class, which in turn has a KitchenObjectSO field
     }
 
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject kitchenObject) // We want to be able to transfor the proper kitchenObject to the new parent clear counter
-    {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject() // We want to check if a clear counter has a kitchen object on top of it
-    {
-        return kitchenObject != null;
-    }
 }
