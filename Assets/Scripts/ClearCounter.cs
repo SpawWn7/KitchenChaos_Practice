375a0f3caede0cdf46fa8add56ebea14b40d5eec
Briant Anaya <briantanaya@yahoo.com>
2024-10-20 12:33:41 -0700

    Fixed crash interaction with containerCounter, added ability for player to pick up objects from counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,7 +9,28 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
+       if(!HasKitchenObject()) // Check if counter does not have a kitchen object (tomato, items, etc)
+        {
+            if (player.HasKitchenObject()) // Player is carrying something
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else // Player is not carrying anything
+            {
+
+            }
+        }
+       else // A kitchen object is already on the counter
+        {
+            if (player.HasKitchenObject()) // Player is carrying something so don't do anything. Don't attempt to pick up kitchen object when player's hands are full. 
+            {
        
+            }
+            else // Player is not carrying anything
+            {
+                GetKitchenObject().SetKitchenObjectParent(player); // Player picks up kitchen object from counter
+            }
+        }
     }
 
 }
