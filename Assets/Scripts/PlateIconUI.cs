b8eb05162642c3608767ef21bd3807849428ce01
Briant Anaya <briantanaya@yahoo.com>
2024-11-07 17:14:45 -0800

    Added icons to plates to indicate what ingredients are currnetly on it

---
+++ Assets/Scripts/PlateIconUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateIconUI : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private Transform iconTemplate;
+
+    private void Awake() // We don't want the icon template to be active at all time. We only want it to be active once we do add an ingredient to a plate.
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual() // We will update the icon visuals as we add more ingredients to the plate
+    {
+        foreach (Transform child in transform) // This is to prevent spawning all the images on the cions provided by the GetKitchenObjectSOList() 
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        {
+            Transform iconTransform = Instantiate(iconTemplate, transform); // We are setting the proper image on the icon
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
+        }
+    }
+}
