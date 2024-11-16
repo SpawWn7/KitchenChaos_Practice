7530f21d561eb377f5e708430046aa595a7ff5dd
Briant Anaya <briantanaya@yahoo.com>
2024-11-15 23:08:58 -0800

    Refined the Game Over UI. Added a clock timer UI to display to the player hoow much time is left.

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -19,6 +19,7 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
 
     private void Awake()
@@ -74,6 +75,7 @@ public class DeliveryManager : MonoBehaviour
                 }
                 if (plateContentsMatchesRecipe) // Player delivered the correct recipe
                 {
+                    successfulRecipesAmount++;
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
@@ -89,5 +91,10 @@ public class DeliveryManager : MonoBehaviour
     public List<RecipeSO> GetWaitingRecipeSOList() 
     {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
     }
 }
